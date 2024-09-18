a0162e815c7792d043a0fcab422a108db50d7695
Alexis Sushko <alexis.sushko@gmail.com>
2024-09-18 15:36:19 -0400

    Made a get request to API for artists, updated state and rendered.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const COHORT = "REPLACE_ME!";
+const COHORT = "2408-Alexis";
 const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/artists`;
 
 // === State ===
@@ -10,6 +10,13 @@ const state = {
 /** Updates state with artists from API */
 async function getArtists() {
   // TODO
+  try {
+    const response = await fetch (API_URL);
+    const responseObj = await response.json();
+    state.artists = responseObj.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 /** Asks the API to create a new artist based on the given `artist` */
@@ -22,6 +29,24 @@ async function addArtist(artist) {
 /** Renders artists from state */
 function renderArtists() {
   // TODO
+  const artistList = document.querySelector("#artists");
+
+  if (!state.artists.length) {
+    artistList.innerHTML = "<li>No artists</li>";
+    return;
+  }
+  
+  
+    const artistsInfo = state.artists.map((artist) => {
+    const info = document.createElement("li");
+    info.innerHTML = `
+    <h2>${artist.name}</h2>
+    <img src= "${artist.imageUrl}" alt="${artist.name}" />
+    <p>${artist.description}</p>
+    ;`
+   return info;
+  });
+  artistList.replaceChildren(...artistsInfo);
 }
 
 /** Syncs state with the API and rerender */
